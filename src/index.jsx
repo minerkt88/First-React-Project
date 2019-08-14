7ed5895646c7bf140ff4cf6a549962a2afd137d0
Kyle Miner <minerkt88@gmail.com>
2019-08-14 11:46:27 -0600

    Move the base files into git repo

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+ReactDOM.render(
+    <div>
+        <h1>Help Queue</h1>
+        <h3>3a</h3>
+        <h3>Tim and Eric</h3>
+        <p>Lost on brownie mountain!</p>
+    </div>,
+    document.getElementById('react-app-root')
+);
